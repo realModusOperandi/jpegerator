09b7a9239272c8a3bfda354a3ecfeb755d76085d
realModusOperandi <liamwestby@icloud.com>
2022-04-13 17:37:16 -0500

    Try to draw an image

--- web/main.js
+++ web/main.js
@@ -1,3 +1,18 @@
-let greeting = () => {
-    console.info("hey");
-}
+let doPage = () => {
+  let canvas = document.createElement('canvas');
+  let context = canvas.getContext('2d');
+
+  let image = document.createElement('img');
+  image.src =
+    'https://i.insider.com/5484d9d1eab8ea3017b17e29?width=600&format=jpeg&auto=webp';
+
+  let result = document.getElementById('result');
+
+  image.onload = () => {
+    context.drawImage(image, 0, 0);
+    result.src = canvas.toDataURL('image/jpeg');
+    console.log('done');
+  };
+
+  console.log('hey');
+};
